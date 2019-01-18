82c23d745b4c3c8ff5f2133744d2cda82fc493eb
Kenny Baas <kenny@baasie.com>
2019-01-18 10:27:49 +0100

    remove local_variables

--- aws/scripts/local_variables.sh
+++ aws/scripts/local_variables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export AWS_ACCOUNT_ID="761563646002"
+export AWS_ACCOUNT_ID=""
 export AWS_REGION="eu-west-1"
-export aws_stage="kb_local"
-export AWS_ACCESS_KEY_ID=AKIAJIKOAOLTKOIY62OA
-export AWS_SECRET_ACCESS_KEY=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
+export aws_stage=""
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
